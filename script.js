f1fca169a2ce13b78259700e060103f9965344f5
Divyansh <divyanshsrivastava20@gmail.com>
2020-12-25 22:08:21 +0530

    final comitt

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// window.addEventListener('keydown', function(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    
-//     if(!audio) return;
-//     audio.currentTime = 0;
-//     audio.play();
-//     key.classList.add('playing');
-// });
-
-// function removeTransition(e) {
-//     if(e.propertyName !== 'transform') return;
-//     this.classList.remove('playing');
-// }
-
-// const keys = document.querySelectorAll('.key');
-// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -34,3 +17,13 @@ const removeTransition = (e) => {
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown',playSound);
+
+const sounds = document.querySelectorAll(".soundy");
+const keysForClick = document.querySelectorAll(".keys div");
+keysForClick.forEach((k, index) => {
+    k.addEventListener("click", function() {
+        k.classList.add('playing')
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+    });
+});
